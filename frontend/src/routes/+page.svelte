bf254c97a3d95b06a18a4c2b85bfc732c922b4ff
Celestin <celestin.meunier@gmail.com>
2026-07-06 19:20:56 +0200

    index

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,5 +1,6 @@
-<h1>Index</h1>
-<p>
-  I'm currently focused on my research on<a href="/cults">cults</a> through conceptual
+<p
+  class="text-2xl text-gray-700 dark:text-gray-300 font-terminal-grotesque prose"
+>
+  I'm currently focused on my research on <a href="/cults">cults</a> through conceptual
   spaces
 </p>
