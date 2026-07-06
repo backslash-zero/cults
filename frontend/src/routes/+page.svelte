e621c35e95092c7f8e1ebbb3737d1946e33e9e87
Celestin <celestin.meunier@gmail.com>
2026-07-06 19:16:57 +0200

    index

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<h1>Index</h1>
+<p>
+  I'm currently focused on my research on<a href="/cults">cults</a> through conceptual
+  spaces
+</p>
---
+++ frontend/src/routes/cults/+page.svelte
@@ -0,0 +1,8 @@
+<p
+  class="text-2xl text-gray-700 dark:text-gray-300 font-terminal-grotesque prose"
+>
+  My research investigates how unstable, contested concepts can be formally
+  defined and represented as semantic spaces—using "cult" as a primary case
+  study. The project originated from asking whether AI psychosis could be
+  understood as a form of sectarian drift.
+</p>
